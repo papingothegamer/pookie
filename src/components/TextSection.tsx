a5bf98b8f0fbe08bae85de4dc99cf358917d4154
papingothegamer <toluwanimidaramola8@gmail.com>
2023-12-09 16:35:35 +0100

    fucking vercel

--- src/components/TextSection.tsx
+++ src/components/TextSection.tsx
@@ -11,12 +11,12 @@ const TextSection: React.FC = () => {
         My stubbornness nearly cost me the one good thing I have in my life, and I am disappointed in myself for failing you.
         There are not enough words I can use to apologize or appease you, so I will let my hands and mind make you a happier woman than you were yesterday.
         I want to see many more Decembers with you and spoil you, treat you right physically and emotionally because you deserve it above everything else.
-        You deserve a love that doesn't make you scared or wary or angry with yourself for pouring back into it.
+        You deserve a love that does not make you scared or wary or angry with yourself for pouring back into it.
         I am sincerely sorry for not being the husband you always needed me to be.
         I was selfish, and it nearly cost me our bond.
         I have owned up to my faults, I will do my humanly possible best to amend them and fix us, and I will personally embark on a journey to find peace with myself so that the peace you give me would be a lot more beautiful.
         I am sorry, my love.
-        I look forward to a brighter future with you as my wife and best friend, and together we'll have the beauty of a life we have dreamed of and brought to reality.
+        I look forward to a brighter future with you as my wife and best friend, and together we will have the beauty of a life we have dreamed of and brought to reality.
         
         I love you, always.
         
